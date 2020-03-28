58413a38afa6782a8d40cd77aa97998f7651ec9f
Robert Conde <elconde@alum.mit.edu>
2020-03-28 16:20:05 -0400

    NY map with state boundaries

--- map.py
+++ map.py
@@ -11,20 +11,24 @@ bbox = [
 ]
 
 # Define the projection, scale, the corners of the map, and the resolution.
-m = Basemap(
+MAP = Basemap(
     lon_0=CENTER_OF_NY[0], lat_0=CENTER_OF_NY[1], width=1000000,
     height=1000000, projection='tmerc'
 )
 
-
-shp_info = m.readshapefile(
+# https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
+MAP.readshapefile(
     'cb_2018_us_county_500k', 'states', drawbounds=True, linewidth=0.45,
+    color='gray'
+)
+MAP.readshapefile(
+    'cb_2018_us_state_500k', 'states', drawbounds=True, linewidth=0.9,
     color='gray'
 )
 
 # Then add element: draw coast line, map boundary, and fill continents:
 # m.drawcoastlines()
-m.drawmapboundary()
+MAP.drawmapboundary()
 # m.fillcontinents()
 
 # You can add rivers as well
