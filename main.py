0fc0cb94c71a8cd8b8031c1ebd4347e351e8a1de
Robert Conde <elconde@alum.mit.edu>
2020-03-28 15:36:54 -0400

    main.py shows NY by county

---
+++ main.py
@@ -0,0 +1,21 @@
+"""View NYTimes Covid-19 data"""
+import matplotlib.pyplot
+import pandas
+import os
+
+CSV_FILE_NAME = os.path.join(os.path.dirname(__file__), 'us-counties.csv')
+
+
+def main():
+    """View NYTimes Covid-19 data"""
+    data_frame = pandas.read_csv(CSV_FILE_NAME)
+    data_frame_ny = data_frame[data_frame['state'] == 'New York']
+    pivot_table = data_frame_ny.pivot_table(
+        values='cases', index='date', columns=['county'], aggfunc=sum
+    )
+    pivot_table.plot(subplots=True, layout=(8, 7), logy=True)
+    matplotlib.pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
