cf6c94a5b869951d6c7a6cf4fa855fd989081f23
Robert Conde <elconde@alum.mit.edu>
2020-04-01 19:24:12 -0400

    Easier NYC plot

--- plot_nyc.py
+++ plot_nyc.py
@@ -9,12 +9,11 @@ CSV_FILE_NAME = os.path.join(os.path.dirname(__file__), 'us-counties.csv')
 def main():
     """View NYTimes Covid-19 data"""
     data_frame = pandas.read_csv(CSV_FILE_NAME)
-    data_frame_ny = data_frame[data_frame['state'] == 'New York']
-    pivot_table = data_frame_ny.pivot_table(
-        values='cases', index='date', columns=['county'], aggfunc=sum
-    )['New York City']
-    print(pivot_table)
-    pivot_table.plot(logy=True)
+    data_frame_ny = (
+        data_frame[data_frame['county'] == 'New York City'][['date', 'cases']]
+    ).set_index('date')
+    print(data_frame_ny)
+    data_frame_ny.plot(logy=True)
     matplotlib.pyplot.show()
 
 
