db2a96969dc365e36bf546361f8e3bc53c414fbc
Robert Conde <elconde@alum.mit.edu>
2020-04-03 11:31:12 -0400

    add averaging window

--- plot_nyc.py
+++ plot_nyc.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot
 import math
 import c19
 
+AVG_WINDOW = 7
+
 
 def main():
     """View NYTimes Covid-19 data"""
     data_frame_ny = c19.get_data_frame_nyc()
     data_frame_ny['doubling rate (days)'] = math.nan
     cases = data_frame_ny['cases']
-    for i in range(1, len(data_frame_ny)):
-        denominator = math.log2(cases.iat[i]/cases[i-1])
+    for i in range(AVG_WINDOW, len(data_frame_ny)):
+        denominator = math.log2(cases.iat[i]/cases[i-AVG_WINDOW])
         if denominator == 0:
             continue
-        data_frame_ny['doubling rate (days)'].iat[i] = 1/denominator
+        data_frame_ny['doubling rate (days)'].iat[i] = AVG_WINDOW/denominator
     print(data_frame_ny)
     data_frame_ny.plot(logy=True)
     matplotlib.pyplot.show()
