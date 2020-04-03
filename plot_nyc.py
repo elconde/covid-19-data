f6497264cfdd19923e25a67f95663723c1bd164f
Robert Conde <elconde@alum.mit.edu>
2020-04-03 14:55:24 -0400

    Better column name

--- plot_nyc.py
+++ plot_nyc.py
@@ -9,13 +9,14 @@ AVG_WINDOW = 7
 def main():
     """View NYTimes Covid-19 data"""
     data_frame_ny = c19.get_data_frame_nyc()
-    data_frame_ny['doubling rate (days)'] = math.nan
+    column_name = 'doubling rate ({} day avg.)'.format(AVG_WINDOW)
+    data_frame_ny[column_name] = math.nan
     cases = data_frame_ny['cases']
     for i in range(AVG_WINDOW, len(data_frame_ny)):
         denominator = math.log2(cases.iat[i]/cases[i-AVG_WINDOW])
         if denominator == 0:
             continue
-        data_frame_ny['doubling rate (days)'].iat[i] = AVG_WINDOW/denominator
+        data_frame_ny[column_name].iat[i] = AVG_WINDOW/denominator
     print(data_frame_ny)
     data_frame_ny.plot(logy=True)
     matplotlib.pyplot.show()
