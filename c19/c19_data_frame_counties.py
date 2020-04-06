7ccbf2f6e3f4d822b65cf189a0249d05e5846ad7
Robert Conde <elconde@alum.mit.edu>
2020-04-06 12:57:10 -0400

    Suffolk County

--- c19/c19_data_frame_counties.py
+++ c19/c19_data_frame_counties.py
@@ -6,4 +6,12 @@ def get_data_frame_counties():
     return pandas.read_csv(c19.CSV_COUNTIES, parse_dates=[0])
 
 
-__all__ = ['get_data_frame_counties']
+def get_data_frame_county(county_name, state_name):
+    counties = get_data_frame_counties()
+    return counties[
+        (counties['county'] == county_name) &
+        (counties['state'] == state_name)
+    ][['date', 'cases', 'deaths']].set_index('date')
+
+
+__all__ = ['get_data_frame_counties', 'get_data_frame_county']
