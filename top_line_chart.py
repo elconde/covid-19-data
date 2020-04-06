7ccf7fbeef9fe38cedacaac88ce2cf54f7fdbe90
Robert Conde <elconde@alum.mit.edu>
2020-04-06 19:52:35 -0400

    N

--- top_line_chart.py
+++ top_line_chart.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot
 import c19
 
+N = 5
 
 def main():
     """View NYTimes Covid-19 data"""
@@ -10,10 +11,12 @@ def main():
     top_ten_states = (
         data_frame[
             data_frame['date'] == max_date
-        ].nlargest(10, 'cases')['state'].values
+        ].nlargest(N, 'cases')['state'].values
     )
     data_frame_plot = data_frame[data_frame['state'].isin(top_ten_states)]
-    pivot_table = data_frame_plot.pivot_table(values='cases', index='date', columns='state')
+    pivot_table = data_frame_plot.pivot_table(
+        values='cases', index='date', columns='state'
+    )
     print(pivot_table)
     pivot_table.plot(logy=True)
     matplotlib.pyplot.show()
