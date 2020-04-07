202befe49489efa669a34337a7d028421b1ddff9
Robert Conde <elconde@alum.mit.edu>
2020-04-06 21:39:16 -0400

    Line chart of user-supplied states

--- chart_by_state.py
+++ chart_by_state.py
@@ -1,4 +1,4 @@
-"""View NYTimes Covid-19 data"""
+"""Line chart of user-supplied states"""
 import matplotlib.pyplot
 import pandas
 import c19
@@ -7,18 +7,8 @@ STATES = ['Massachusetts', 'Pennsylvania', 'New York', 'New Jersey']
 MULTIPLIER = 1e6
 
 
-def get_state_population(statistics, state):
-    """Get the population of the state"""
-    state_abbr = c19.parse_state_name(state)
-    return (
-        statistics[
-            statistics['State'] == state_abbr
-        ]['Population (2010)'].sum()
-    )
-
-
 def main():
-    """View NYTimes Covid-19 data"""
+    """Line chart of user-supplied states"""
     data_frame = pandas.read_csv(c19.CSV_STATES)
     statistics = c19.get_county_statistics()
     data_frame_chart = data_frame[data_frame['state'].isin(STATES)]
@@ -26,7 +16,7 @@ def main():
         values='cases', index='date', columns=['state'], aggfunc=sum
     )
     for state in STATES:
-        population = get_state_population(statistics, state)
+        population = c19.get_state_population(statistics, state)
         pivot_table[state] = pivot_table[state] / population * MULTIPLIER
     print(pivot_table)
     pivot_table.plot(logy=True)
