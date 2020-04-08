e63ec3a8b05fff7632c54d5d925a6e6a1d19604b
Robert Conde <elconde@alum.mit.edu>
2020-04-08 17:30:12 -0400

    Replace brackets

--- c19/c19_constants.py
+++ c19/c19_constants.py
@@ -3,10 +3,11 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 CSV_COUNTIES = os.path.join(ROOT_DIR, 'us-counties.csv')
 CSV_STATES = os.path.join(ROOT_DIR, 'us-states.csv')
+CSV_STATISTICS = os.path.join(ROOT_DIR, 'statistics.csv')
 FIPS_OUTER_BOROUGHS = (36085, 36081, 36047, 36005)
 FIPS_MANHATTAN = 36061
 
 __all__ = [
     'ROOT_DIR', 'CSV_COUNTIES', 'CSV_STATES', 'FIPS_OUTER_BOROUGHS',
-    'FIPS_MANHATTAN'
+    'FIPS_MANHATTAN', 'CSV_STATISTICS'
 ]
--- c19/c19_county_statistics.py
+++ c19/c19_county_statistics.py
@@ -1,11 +1,13 @@
 """Read the county statistics file"""
-
+import c19
 import pandas
 
 
 def get_county_statistics():
     """Read the county statistics into a data frame."""
-    return pandas.read_csv('statistics.csv', thousands=',')
+    data_frame = pandas.read_csv(c19.CSV_STATISTICS, thousands=',')
+    data_frame['County'].replace(r' \[.*', '', inplace=True, regex=True)
+    return data_frame
 
 
 __all__ = ['get_county_statistics']
