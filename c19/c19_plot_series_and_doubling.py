1258d3ff65d49a34446d9bc4dc898fdbbbf8dd16
Robert Conde <elconde@alum.mit.edu>
2020-04-11 08:00:55 -0400

    Allow diff plotting

--- c19/c19_plot_series_and_doubling.py
+++ c19/c19_plot_series_and_doubling.py
@@ -7,7 +7,9 @@ def plot_series_and_doubling(data_frame, series_name, avg_window, logy=True):
     assert series_name in ('cases', 'deaths', 'diff')
     column_name = 'doubling rate ({} day avg.)'.format(avg_window)
     data_frame[column_name] = math.nan
+    to_drop = ['cases', 'deaths']
     if series_name == 'diff':
+        to_drop.append('diff')
         data_frame['diff'] = data_frame['cases'] - data_frame['deaths']
     series = data_frame[series_name]
     len_ = len(data_frame)
@@ -18,7 +20,6 @@ def plot_series_and_doubling(data_frame, series_name, avg_window, logy=True):
         doubling_rate = avg_window / denominator
         data_frame[column_name].iat[i] = doubling_rate
     print(data_frame)
-    to_drop = ['cases', 'deaths', 'diff']
     to_drop.remove(series_name)
     data_frame.drop(labels=to_drop, axis=1, inplace=True)
     print(data_frame)
