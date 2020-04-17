a92eb148c30a0a14afd73197f2ce6778acfa5419
Robert Conde <elconde@alum.mit.edu>
2020-04-17 13:59:44 -0400

    No need for special NYC data frame

--- c19/__init__.py
+++ c19/__init__.py
@@ -1,6 +1,5 @@
 from .c19_constants import *
 from .c19_data_frame_counties import *
-from .c19_data_frame_nyc import *
 from .c19_postal import *
 from .c19_county_statistics import *
 from .c19_population import *
--- plot_nyc.py
+++ plot_nyc.py
@@ -6,7 +6,10 @@ AVG_WINDOW = 7
 
 def main():
     """View NYTimes Covid-19 data"""
-    c19.plot_series_and_doubling(c19.get_data_frame_nyc(), 'cases', AVG_WINDOW)
+    c19.plot_series_and_doubling(
+        c19.get_data_frame_county('New York City', 'New York'),
+        'cases', AVG_WINDOW
+    )
 
 
 if __name__ == '__main__':
