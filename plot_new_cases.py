1a9f84bd78e322bfd0e73f2c29705a86376dd695
Robert Conde <elconde@alum.mit.edu>
2020-04-19 21:31:13 -0400

    New cases command line arguments

--- plot_new_cases.py
+++ plot_new_cases.py
@@ -1,17 +1,30 @@
 """Plot new cases for a given county"""
+import argparse
+
 import c19
 import matplotlib.pyplot
 
-AVG_WINDOW = 7
+
+def parse_args():
+    """Parse the command line arguments"""
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--county', default='New York City', help='County')
+    parser.add_argument('--state', default='New York', help='State')
+    parser.add_argument('--window', default=7, help='Avg. Window', type=int)
+    return parser.parse_args()
 
 
 def main():
     """Plot new cases for a given county"""
-    data_frame = c19.get_data_frame_county('New York City', 'New York')
-    column_name = 'new cases ({} day avg.)'.format(AVG_WINDOW)
+    args = parse_args()
+    data_frame = c19.get_data_frame_county(args.county, args.state)
+    column_name = 'new cases ({} day avg.)'.format(args.window)
     data_frame[column_name] = (
-        data_frame['cases'] - data_frame['cases'].shift(AVG_WINDOW)
-    ) / AVG_WINDOW
+        data_frame['cases'] - data_frame['cases'].shift(args.window)
+    ) / args.window
     data_frame[column_name].dropna().plot()
     print(data_frame[column_name])
     matplotlib.pyplot.show()
