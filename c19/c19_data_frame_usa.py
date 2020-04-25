70d3db9bf70e6967d7c237b798bdbc8fc25de541
Robert Conde <elconde@alum.mit.edu>
2020-04-24 21:59:54 -0400

    Wrong CSV

--- c19/c19_data_frame_usa.py
+++ c19/c19_data_frame_usa.py
@@ -3,7 +3,7 @@ import c19
 
 
 def get_data_frame_usa():
-    return pandas.read_csv(c19.CSV_STATES, parse_dates=[0]).set_index('date')
+    return pandas.read_csv(c19.CSV_USA, parse_dates=[0]).set_index('date')
 
 
 __all__ = ['get_data_frame_usa']
