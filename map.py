33db19527604573c5c7588a88be9239c1505c81e
Robert Conde <elconde@alum.mit.edu>
2020-04-26 12:01:46 -0400

    Simpler map.py

--- map.py
+++ map.py
@@ -9,9 +9,7 @@ import logging
 import os
 from PIL import Image
 import matplotlib.pyplot
-import pandas
 import datetime
-import argparse
 import math
 
 ROOT_DIR = os.path.dirname(__file__)
@@ -23,7 +21,6 @@ START_DATE = datetime.date(2020, 3, 1)
 CENTER = [39.828175, -98.5795]
 WIDTH = 6.5e6
 HEIGHT = 4e6
-CSV_FILE_NAME = os.path.join(ROOT_DIR, 'us-counties.csv')
 PNG_DIR = os.path.join(ROOT_DIR, 'png')
 DPI = 300
 
@@ -76,7 +73,7 @@ def get_number_of_cases(fips, data_frame, date):
     return data_frame_result['cases'].values[0]
 
 
-def draw_map(args):
+def draw_map():
     """Draw the map!"""
     # Define the projection, scale, the corners of the map, and the resolution.
     base_map = mpl_toolkits.basemap.Basemap(
@@ -88,7 +85,7 @@ def draw_map(args):
         linewidth=0.2,
         color='gray'
     )
-    data_frame = get_data_frame()
+    data_frame = c19.get_data_frame_counties()
     old_scatter = None
     old_text = None
     max_date = data_frame['date'].max()
@@ -113,11 +110,7 @@ def draw_map(args):
                 continue
             lons.append(statistic['Longitude'])
             lats.append(statistic['Latitude'])
-            if args.scale_by_population:
-                scalar = BUBBLE_SCALE / c19.get_county_population(fips, statistics)
-            else:
-                scalar = 0.10
-            cases.append(number_of_cases * scalar)
+            cases.append(number_of_cases / BUBBLE_SCALE)
         if old_text:
             old_text.remove()
         old_text = matplotlib.pyplot.text(
@@ -131,15 +124,6 @@ def draw_map(args):
         )
         matplotlib.pyplot.savefig(os.path.join(PNG_DIR, file_name), dpi=DPI)
         date += datetime.timedelta(days=1)
-
-
-def get_data_frame():
-    """Get the data frame from the csv file"""
-    data_frame = pandas.read_csv(CSV_FILE_NAME)
-    data_frame['date'] = pandas.to_datetime(
-        data_frame['date'], format='%Y-%m-%d'
-    )
-    return data_frame
 
 
 def create_gif():
@@ -167,26 +151,16 @@ def get_pngs():
     ]
 
 
-def parse_args():
-    """Parse the command line arguments"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--scale-by-population', action='store_true')
-    parser.add_argument('--verbose', action='store_true')
-    return parser.parse_args()
-
-
-def setup_logger(args):
+def setup_logger():
     """Set up the logger"""
     logging.basicConfig(
-        level=logging.DEBUG if args.verbose else logging.INFO,
-        format="%(asctime)s %(name)s %(message)s"
+        level=logging.NOTSET, format="%(asctime)s %(name)s %(message)s"
     )
 
 
 def main():
-    args = parse_args()
-    setup_logger(args)
-    draw_map(args)
+    setup_logger()
+    draw_map()
     create_gif()
 
 
