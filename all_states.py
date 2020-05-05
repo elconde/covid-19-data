68d52b8a169a58c56172365064d639ebc052da2f
Robert Conde <elconde@alum.mit.edu>
2020-05-05 18:22:22 -0400

    All States shows daily rate

--- all_states.py
+++ all_states.py
@@ -8,6 +8,7 @@ def parse_args():
     """Parse the command line arguments"""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--scale-by-population', action='store_true')
+    parser.add_argument('--window', default=7)
     return parser.parse_args()
 
 
@@ -15,10 +16,11 @@ def main():
     """View NYTimes Covid-19 data"""
     args = parse_args()
     pivot_table = c19.get_pivot_table_states(args.scale_by_population)
+    pivot_table = (pivot_table - pivot_table.shift(args.window)) / args.window
     latest_row = pivot_table.iloc[-1]
     pivot_table.plot(
         logy=True, subplots=True, layout=(8, 8),
-        ylim=(latest_row.min(), latest_row.max())
+        ylim=(pivot_table.min().min(), pivot_table.max().max()*10)
     )
     print(latest_row)
     matplotlib.pyplot.show()
