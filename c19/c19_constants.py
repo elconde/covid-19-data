83f8dd012b846b779eb7090c2b4a71529ecad7bd
Robert Conde <elconde@alum.mit.edu>
2020-08-20 13:55:25 -0400

    Point to NYT folder

--- c19/c19_constants.py
+++ c19/c19_constants.py
@@ -1,10 +1,10 @@
 import os
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-CSV_COUNTIES = os.path.join(ROOT_DIR, 'us-counties.csv')
-CSV_STATES = os.path.join(ROOT_DIR, 'us-states.csv')
+CSV_COUNTIES = os.path.join(ROOT_DIR, 'nyt/us-counties.csv')
+CSV_STATES = os.path.join(ROOT_DIR, 'nyt/us-states.csv')
 CSV_STATISTICS = os.path.join(ROOT_DIR, 'statistics.csv')
-CSV_USA = os.path.join(ROOT_DIR, 'us.csv')
+CSV_USA = os.path.join(ROOT_DIR, 'nyt/us.csv')
 
 __all__ = [
     'ROOT_DIR', 'CSV_COUNTIES', 'CSV_STATES', 'CSV_STATISTICS', 'CSV_USA'
